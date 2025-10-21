cf5b09731dec40f240727f09b98e8c2c57a1d911
oriiquesillva <l.sillvasouza@gmail.com>
2025-10-21 07:02:36 -0300

    Implementção do controller de projetos e criaçao da rota para obtenção de todos os projetos.

---
+++ OrdoTasks/Controllers/ProjetosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrdoTasksApplication.Interfaces;
+
+namespace OrdoTasks.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjetosController : Controller
+    {
+        private readonly IProjetoRepository _projetoRepository;
+
+        public ProjetosController(IProjetoRepository projetoRepository)
+        {
+            _projetoRepository = projetoRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProjects()
+        {
+            var projetos = await _projetoRepository.GetAllAsync();
+
+            return Ok(projetos);
+        }
+    }
+}
