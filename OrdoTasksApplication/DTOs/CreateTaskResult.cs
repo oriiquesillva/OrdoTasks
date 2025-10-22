af5b0ba3d03088401c369ffd4b8fef6649e0f680
oriiquesillva <l.sillvasouza@gmail.com>
2025-10-22 01:47:53 -0300

    Criaçao do DTO para o retorno do insert da tarefa

---
+++ OrdoTasksApplication/DTOs/CreateTaskResult.cs
@@ -0,0 +1,15 @@
+﻿using OrdoTasksDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdoTasksApplication.DTOs
+{
+    public class CreateTaskResult
+    {
+        public int Id { get; set; }
+        public Tarefa Tarefa { get; set; }
+    }
+}
